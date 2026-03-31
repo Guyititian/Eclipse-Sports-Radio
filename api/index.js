8c57b81bd80e5e0d0b58cb0af16b7cb2df1f71d4
Guyititian <128245262+Guyititian@users.noreply.github.com>
2026-03-30 22:53:03 -0400

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,5 @@
+module.exports = function(req, res) {
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Content-Type", "application/json");
+res.status(200).json({ ok: true });
+};
